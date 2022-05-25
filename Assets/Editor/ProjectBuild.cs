4582124e600c9ddf1ea0879323a71f3cc45c1e06
n-fridman <n.fridman3116@gmail.com>
2022-05-25 15:09:59 +0400

    Update build script.

--- Assets/Editor/ProjectBuild.cs
+++ Assets/Editor/ProjectBuild.cs
@@ -1,10 +1,32 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace
 {
     public static class ProjectBuild
     {
         public static void BuildForAndroid()
         {
-            
+            var outdir = Environment.CurrentDirectory + "/Builds/Android";
+            var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+
+            // Обработка папки
+            if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+            if (File.Exists(outputPath)) File.Delete(outputPath);
+
+            // Запускаем проект в один клик
+            string[] scenes = {"Assets/BurningLab/SwipeDetector/Examples/Scenes/SwipeInputDemoScene.unity"};
+            BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+            if (File.Exists(outputPath))
+            {
+                Debug.Log("Build Success :" + outputPath);
+            }
+            else
+            {
+                Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+            }
         }
     }
 }
