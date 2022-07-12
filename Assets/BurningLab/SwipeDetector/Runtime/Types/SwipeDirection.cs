60db55724b04e824d6a79cdc159c53e118fa546c
n.fridman <n.fridman@burning-lab.com>
2022-07-13 03:18:23 +0400

    [SI-26] Create swipe direction enumeration.

---
+++ Assets/BurningLab/SwipeDetector/Runtime/Types/SwipeDirection.cs
@@ -0,0 +1,28 @@
+﻿namespace BurningLab.SwipeDetector.Types
+{
+    /// <summary>
+    /// Swipe direction enumeration.
+    /// </summary>
+    public enum SwipeDirection
+    {
+        /// <summary>
+        /// Up swipe.
+        /// </summary>
+        Up,
+        
+        /// <summary>
+        /// Left swipe.
+        /// </summary>
+        Left,
+        
+        /// <summary>
+        /// Down swipe.
+        /// </summary>
+        Down,
+        
+        /// <summary>
+        /// Right swipe.
+        /// </summary>
+        Right
+    }
+}
